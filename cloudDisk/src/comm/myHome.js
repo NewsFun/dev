c9cec73f05f1b37bc07fc4a62cb90263538c6ac6
newsfun <80733049@qq.com>
2016-09-26 17:42:26 +0800

    文件树

--- cloudDisk/src/comm/myHome.js
+++ cloudDisk/src/comm/myHome.js
@@ -11,28 +11,26 @@ $(document).ready(function(){
     function Disk(){}
     Disk.prototype = {
         init:function(){
-            this.initEast();
+            var self = this;
+            self.initEast();
+            self.initCbox();
         },
         initEast:function(){
-            var self = this;
-            var pd = self._getPadding(EAST);
             var sh = ~~SOUTH.height(),
-                ew = W-WEST.width()-pd[3]-pd[1],
-                eh = H-NORTH.height()-sh-pd[0]-pd[2],
+                ew = W-WEST.width(),
+                eh = H-NORTH.height()-sh,
                 wh = H-NORTH.height()-sh;
             WEST.css('height', wh+'px');
             EAST.css('width', ew+'px').css('height', eh+'px');
         },
-        _getPadding:function(jquery){
-            var pd = jquery.css('padding').split(' ');
-            //console.log(pd);
-            var ps = {};
-            ps[0] = parseFloat(pd[0]);
-            ps[1] = isNaN(parseFloat(pd[1]))?ps[0]:parseFloat(pd[1]);
-            ps[2] = isNaN(parseFloat(pd[2]))?ps[0]:parseFloat(pd[2]);
-            ps[3] = isNaN(parseFloat(pd[3]))?ps[1]:parseFloat(pd[3]);
-            //console.log(ps);
-            return ps;
+        initCbox:function(){
+            $('#selectAll').on('click',function(){
+                var check = $(this).is(':checked');
+                console.log(check);
+                $('.tb-checkbox').each(function(){
+                    $(this).attr('checked', check);
+                });
+            });
         }
     };
 
--- cloudDisk/src/tree.js
+++ cloudDisk/src/tree.js
@@ -2,60 +2,97 @@
  * Created by bobo on 2016/9/19.
  */
 (function(){
-    function Tree(){
+    function Tree(con){
         this.title = $('.tree_title');
-        this.baseH = $('.tree_content li').css('line-height');
-        this.openClass = 'icon_hollow';
-        this.closeClass = 'icon_solid';
-        this.level = 0;
+        this.baseH = con.baseH;
+        this.openClass = con.openClass;
+        this.closeClass = con.closeClass;
+        this.target = null;
     }
     Tree.prototype = {
         init:function(){
             this.openTree();
+            //this.initCookie();
         },
         openTree:function(){
             var self = this;
-            self.title.on('click', function(){
-                var title = $(this).children('.tree_icon');
-                if(title.hasClass(self.closeClass)){
-                    title.removeClass(self.closeClass).addClass(self.openClass).html('-');
-                    $(this).parent().removeAttr('style');
-                    if(!$(this).data('opened')){
-                        //var url = 'http://localhost.home.news.cn:8080/xhVdisk2/vdisk/control/address/get_child_address.do';
-                        //self._getData(url);
-                        $(this).data('opened', 'true');
-                    }
-                }else{
-                    title.removeClass(self.openClass).addClass(self.closeClass).html('+');
-                    $(this).parent().css('height', self.baseH);
-                }
-                self._getPath($(this));
+            self.title.each(function(){
+                if($(this).addOpen) return true;
+                $(this).on('click', function(){
+                    self._nodeClick($(this));
+                    $(this).addOpen = true;
+                });
             });
         },
-        _getData:function(url, callback){
-            $.ajax({
-                url:url,
-                type:'get',
-                dataType:'json',
-                success:function(data){
-                    console.log(data);
-                    if(callback) callback(data);
-                }
-            });
+        _getData:function(url, pid){
+            var self = this;
+            $.get(url, function(data){
+                //console.log(data);
+                self._addHtml(data, pid);
+            },'json');
         },
         _operate:function(){
-            var names = $('tree_name')
+            var names = $('tree_name');
+        },
+        _nodeClick:function(node){
+            var self = this;
+            var pid = node.data('id');
+            var title = node.children('.tree_icon');
+            if(title.hasClass(self.closeClass)){
+                title.removeClass(self.closeClass).addClass(self.openClass).html('-');
+                node.parent().removeAttr('style');
+                if(!$(this).data('opened')){
+                var url = '/json/tree'+pid+'.json';
+                //var url = 'http://localhost.home.news.cn:8080/xhVdisk2/vdisk/control/address/get_child_address.do?parentId='+pid;
+                self._getData(url, pid);
+                node.data('opened', 'true');
+                }
+            }else{
+                title.removeClass(self.openClass).addClass(self.closeClass).html('+');
+                node.parent().css('height', self.baseH);
+            }
+            self._getPath(node);
         },
         _getPath:function(node){
             var parents = node.parents('.tree_branch');
-            var path = '';
+            var path = [];
             for(var i = parents.length-1;i>=0;i--){
                 var p = $(parents[i]).find('.tree_name')[0];
-                path += $(p).text();
+                path.push($(p).text());
             }
-            console.log(path);
+            return path;
+        },
+        _addHtml:function(data, pid){
+            var self = this;
+            var html = '';
+            for(var i = 0;i<data.length;i++){
+                if(data[i].user){
+                    html += '<li>' +
+                    '<div class="tree_title" data-id="'+data[i].id+'">' +
+                    '<span class="tree_name">'+data[i].name+'</span>' +
+                    '</div>' +
+                    '<ul id="'+data[i].id+'" class="tree_branch"></ul></li>';
+                }else{
+                    html += '<li>' +
+                    '<div class="tree_title" data-id="'+data[i].id+'">' +
+                    '<span class="tree_icon icon_solid">+</span><span class="tree_name">'+data[i].name+'</span>' +
+                    '</div>' +
+                    '<ul id="'+data[i].id+'" class="tree_branch"></ul></li>';
+                }
+            }
+            $('#'+pid).html(html);
+
+            self.title = $('.tree_title');
+            self.openTree();
+        },
+        initCookie:function(){
+            var cookie = document.cookie;
+            if(cookie.length>0){
+                var ticket = cookie.indexOf('ticket');
+            }else{
+                console.log('no cookie');
+            }
         }
     };
-
-    new Tree().init();
+    window.Tree = Tree;
 })();
