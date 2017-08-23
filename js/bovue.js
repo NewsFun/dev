1f28c38e4547feac1a5bcf58dd324e771bfdb402
newsfun <80733049@qq.com>
2017-08-23 17:15:05 +0800

    字符串解析

--- js/bovue.js
+++ js/bovue.js
@@ -1,11 +1,29 @@
-(function (window, $) {
+(function (window, $, undefined) {
+	var vue = 'getAndRemoveAttr';
 	var tobject = {
 		title:'hello world',
 		name :'world',
 		link :'http://www.news.cn',
 		content:['1','2','3','4']
 	}
-	function stringDetection(){
-		
+	var len = 0,
+		index = 0;
+	function parseString(str){
+		len = str.length;
+		index = str.indexOf('<');
+		console.log(index);
+	}
+	function goon(){
+		return index<len;
+	}
+
+	function stringDetection(string){
+		var attr = getQueryString(string,'bo-if');
+	}
+	function getQueryString(string, name){
+	    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+	    var r = string.substr(1).match(reg);
+	    if(r!==null) return decodeURIComponent(r[2]);
+	    return null;
 	}
 })(window, jQuery);
