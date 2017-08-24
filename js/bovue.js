cafc84bc10c710cf80dfbac81f709af4a6b9ccfe
newsfun <80733049@qq.com>
2017-08-24 17:12:08 +0800

    解析字符串

--- js/bovue.js
+++ js/bovue.js
@@ -6,12 +6,16 @@
 		link :'http://www.news.cn',
 		content:['1','2','3','4']
 	}
+	var teststr = '<div><a href="www.baidu.com"></a></div>'
 	var len = 0,
-		index = 0;
-	function parseString(str){
+		index = 0,
+		tags = [],
+		domtree = {};
+	function parse(str){
 		len = str.length;
-		index = str.indexOf('<');
-		console.log(index);
+		tags = str.split('<');
+		console.log(tags);
+		parseString(tags[2]);
 	}
 	function goon(){
 		return index<len;
@@ -26,4 +30,37 @@
 	    if(r!==null) return decodeURIComponent(r[2]);
 	    return null;
 	}
+	function parseString(str){
+		if(str.length<1) return false;
+		/*
+		str = str.replace(/"/g,'').replace(/>/g,'"}');
+		if(str.charCodeAt(0)===47){
+			str = str.replace('/','{"end":"');
+		}else{
+			str = '{"start":"'+str.replace(/=/g,'":"').replace(/\s/g,'","');
+		}
+		return JSON.parse(str);
+		*/
+		var strobj = {};
+		var slen = str.length;
+		var ios = 0;
+		var tag = '';
+		var cur = '';
+		while(ios<slen){
+			swith(str.charCodeAt(ios)){
+				case 60:
+					cur = 'tag';
+					break;
+				case 47:
+					if(cur==='tag') cur='end';
+					break;
+				case:
+					break;
+				default:
+					tag+=str[ios];
+					break;
+			}
+		}
+	}
+	parse(teststr);
 })(window, jQuery);
