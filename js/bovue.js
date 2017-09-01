0d4cd63b7a2d8673b2c65a485e9eecd3cf313d04
newsfun <80733049@qq.com>
2017-09-01 17:59:19 +0800

    dom测试

--- js/bovue.js
+++ js/bovue.js
@@ -7,19 +7,21 @@
 		link :'http://www.news.cn',
 		content:['1','2','3','4']
 	};
-	var teststr = '<div><a href="www.baidu.com">百度</a></div>';
+	var teststr = '<div class="test" bo-if=hello><a href="www.baidu.com">百度</a></div>';
 	var len = 0,
 		index = 0,
 		tags = [],
 		domtree = {};
 	function setModelBox() {
 		var body = doc.querySelector('body');
-		var script = creatEl('script',{
+		var script = creatEl('div',{
 			'type':'text/html',
 			'id':'bo-mod',
+			'style':'display:none;',
 			'innerHTML':teststr
 		});
-		body.appendChild(script);
+		body.append(script);
+		getChildren();
 	}
 	function creatEl(tag, attr) {
 		var el = doc.createElement(tag);
@@ -27,6 +29,15 @@
 			el[i] = attr[i];
 		}
 		return el;
+	}
+	function getChildren() {
+		var script = doc.querySelector('#bo-mod');
+		// console.log(script);
+		var child = script.children;
+		console.log(child);
+		/*for(var i in child){
+			console.log(i);
+		}*/
 	}
 	function getQueryString(string, name){
 	    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
