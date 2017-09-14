0078c8f5c80cc6159fa233428395e7f5f0e81b3d
newsfun <80733049@qq.com>
2017-09-14 17:16:55 +0800

    时间差函数

--- js/calendar.js
+++ js/calendar.js
@@ -3,9 +3,15 @@
 	var calendar = $('#calendar');
 	var days = getDays(80);
 	var age = 27;
+	var now = new Date();
+	var born = '2000/1/1 00:00:00';
+	function test() {
+		var bday = new Date(born).getTime();
+		var date = now.getTime();
+		console.log(bday);
+	}
 	function getProportion(age) {
 		var pass = getDays(age);
-		console.log(pass);
 		var divs = calendar.children('div');
 		divs.each(function (k,v) {
 			if(k<pass) $(this).addClass('passed');
@@ -24,6 +30,13 @@
 		var exd = 365+exh/24;
 		return Rd(exd*years);
 	}
-	createGrid(days);
-	getProportion(age);
+	function timeDelta(start, end) {
+		if(!end) end = new Date();
+		var st = new Date(start).getTime();
+		var et = new Date(end).getTime();
+		var td = et-st;
+	}
+	// createGrid(days);
+	// getProportion(age);
+	test();
 })(window,jQuery);
