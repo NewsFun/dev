c0642e30c3979d5f4d7f0492fbf901d46d9287f0
newsfun <80733049@qq.com>
2017-09-28 15:44:44 +0800

    对应dom树

--- js/base.js
+++ js/base.js
@@ -1,22 +1,15 @@
 (function (win,$,undefined) {
-	var modList = [];
+	var modList = {};
+	var current = {};
+	var index = 0;
 	var xhfor = 'xh-for';
-	var teststr = '<div class="info-unfold" id="J-info-unfold" xh-for="e">abcdefg<em class="layout-icons layout-icons_down-arrow"></em></div>';
+	var teststr = '<div class="info-unfold" id="J-info-unfold" xh-for="e"><em class="layout-icons_down-arrow"></em><em class="icons_down-arrow"></em><em class="layout-icons"></em></div>';
 	var endt = '</em></div>';
-	var testd = {
-		a:'a',
-		b:'b',
-		c:'c',
-		d:'d',
-		e:[{
-			e1:'e1',
-			e2:'e2'
-		}]
-	};
+	var testd = {a:'a',b:'b',c:'c',d:'d',e:[{e1:'e1',e2:'e2'}]};
 
-	var reg = /\{\{((?:.|\n)+?)\}\}/g;
-	var tagname = /<\s*(\w+)/g;
-	var endtag = /<\s*\//g;
+	var reg = /\{\{((?:.|\n)+?)\}\}/;
+	var tagname = /<\s*(\w+)/;
+	var endtag = /<\s*\/\s*(\w+)/;
 	var forexp = attrRegExp(xhfor);
 	var subtag = /<[^<]*/g;
 
@@ -33,22 +26,49 @@
 			text = text.replace(c[0], data[c[1]]);
 		}
 	}
-	function getForStr(str) {
-		var fe = forexp.exec(str);
-		// console.log(fe);
-		if(fe === null) return;
-		var tn = tagname.exec(str)[1];
-		modList.push();
+	function getForStr(str, obj) {
+		// console.log(obj);
+		current = obj;
+		var tn = tagname.exec(str)[1];/*get tag name*/
+		console.log(index);
+		obj[index] = {
+			// _par:obj,
+			_tag:tn,
+			_str:str
+		};
+		var fe = forexp.exec(str);/*for*/
+		if(fe === null) return obj[index];
+		obj[index]._for = fe[1];
+		return obj[index];
 	}
-	
+	function getEndMod(str, obj) {
+		var tr = tagname.exec(str);
+		console.log(tr);
+		// return obj._par;
+	}
 	function attrRegExp(attr) {
 		// var main = "\\s*=\\s*('([^']*)'|\"([^\"]*)\")";
-		return new RegExp(attr+"\\s*=\\s*\"([^\"]*)\"","g");
+		return new RegExp(attr+"\\s*=\\s*\"([^\"]*)\"","i");
 	}
-	function parseStr(str) {
-		var substr = subtag.exec(str);
-		getForStr(substr);
+	function parseStr(str, obj) {
+		var sub = subtag.exec(str);
+		if(sub === null){
+			console.log(modList);
+			return;
+		}
+		var substr = sub[0];
+		var et = endtag.exec(substr);
+		// console.log(et);
+		if(et === null){
+			var subobj = getForStr(substr, obj);
+			index += 1;
+			parseStr(str, subobj);
+		}else{
+			index+=1;
+			parseStr(str, current);
+		}
 	}
-	parseStr(teststr);
+	parseStr(teststr, modList);
+	
 
 })(window, jQuery);
