7c221e6a2b75db7f031bb8b7ebec7708e99d17c1
newsfun <80733049@qq.com>
2017-10-09 17:55:16 +0800

    添加if选项

--- js/base.js
+++ js/base.js
@@ -34,15 +34,22 @@
 	var index = 0;
 	var modList = {};
 	var xhfor = 'xh-for';
+	var xhif = 'xh-if';
 	var singletag = 'input,img,link,meta,';
 
-	var regexp = /\{\{((?:.|\n)+?)\}\}/g;
+	var regexp = /\{\{((?:.|\n)+?)\}\}/m;
 	var subtagexp = /<[^<]*/g;
 	var tagnamexp = /<\s*([a-zA-Z]+)/;
 	var endtagexp = /<\s*\/\s*(\w+)/;
 	var forexp = getAttrExp(xhfor);
+	var ifexp = getAttrExp(xhif);
 	var isArray = _is('Array');
-
+	win.bo = {
+		"Bo":Bo,
+		"mod2Dom":mod2Dom,
+		"parseStr":parseStr,
+		"dataInject":dataInject
+	};
 	function dataInject(text, data){
 		while(true){
 			var c = regexp.exec(text);
@@ -52,23 +59,38 @@
 	}
 	function getForStr(str, obj) {
 		var tn = tagnamexp.exec(str)[1];/*get tag name*/
-		// console.log(index);
 		obj[index] = {
 			_par:obj,
 			_tag:tn,
 			_str:str
 		};
 		var fe = forexp.exec(str);/*for*/
-		if(fe === null) return obj[index];
-		obj[index]._for = fe[1];
+		var ie = ifexp.exec(str);
+		if(fe !== null) obj[index]._for = fe[1];
+		if(ie !== null) obj[index]._if = ie[1];
 		return obj[index];
 	}
 	function getAttrExp(attr) {
 		// var main = "\\s*=\\s*('([^']*)'|\"([^\"]*)\")";
 		return new RegExp(attr+"\\s*=\\s*\"([^\"]*)\"","i");
 	}
+	function $(el) {
+		var mel = document.querySelectorAll(el);
+		if(mel.length<2) return mel[0];
+		return mel;
+	}
+	function Bo(el, data) {
+		if(!modList[el]){
+			var inhtml = $(el).innerHTML;
+			index = 0;
+			modList[el] = parseStr(inhtml, {});
+		}
+		$(el).innerHTML = mod2Dom(modList[el], data);
+		return bo;
+	}
 	function parseStr(str, obj) {
 		var sub = subtagexp.exec(str);
+		// console.log(sub);
 		if(sub === null) return obj;
 		var substr = sub[0];
 		var subobj = obj._par;
@@ -100,6 +122,9 @@
 		for (var i = 0;i<keys.length;i++) {
 			if(keys[i].indexOf('_')>-1) continue;/*if this attr is undom continue*/
 			value = mod[keys[i]];
+			if(value._if){
+				if(!data[value._if]) continue;
+			}
 			html += dataInject(value._str, data);/*data inject into start tag*/
 			if(singletag.indexOf(value._tag+',')<0) endtag = '</'+value._tag+'>';/*if this tag is not single tag*/
 			if(value._for) data = data[value._for];/*if tag has 'for' attr*/
