2f4b6eb433fc0e34b666f2420c00d8b1dc67f0bd
newsfun <80733049@qq.com>
2017-10-10 16:48:36 +0800

    删除Dev文件，备份旧for方法

--- js/bovue.js
+++ js/bovue.js
@@ -1,51 +1,142 @@
-(function (win, $, undefined) {
-	var vue = 'getAndRemoveAttr';
-	var doc = win.document;
-	var tobject = {
-		title:'hello world',
-		name :'world',
-		link :'http://www.news.cn',
-		content:['1','2','3','4']
+(function (win,$,undefined) {
+	'use strict';
+	var teststr = $x('#test').innerHTML;
+	var endt = '</em></div>';
+	var testd = {
+		a:'a',
+		b:'b',
+		c:'c',
+		d:'d',
+		e:[{
+			e1:[{
+				'e10':1
+			},{
+				'e10':2
+			}],
+			e2:'e2'
+		},{
+			e1:[{
+				'e10':3
+			},{
+				'e10':4
+			}],
+			e2:'e2'
+		},{
+			e1:[{
+				'e10':5
+			},{
+				'e10':6
+			}],
+			e2:'e21'
+		}]
 	};
-	var teststr = '<div class="test" bo-if=hello><a>阿里</a><a href="www.baidu.com">百度</a></div>';
-	var len = 0,
-		index = 0,
-		tags = [],
-		domtree = {};
-	function setModelBox() {
-		var body = doc.querySelector('body');
-		var script = creatEl('div',{
-			'type':'text/html',
-			'id':'bo-mod',
-			'style':'display:none;',
-			'innerHTML':teststr
-		});
-		body.append(script);
-		getChildren();
-	}
-	function creatEl(tag, attr) {
-		var el = doc.createElement(tag);
-		for(var i in attr){
-			el[i] = attr[i];
-		}
-		return el;
-	}
-	function getChildren() {
-		var script = doc.querySelector('#bo-mod');
-		var child = script.childNodes;
-		// console.log(child);
-		ifModAttr(child);
-	}
-	function ifModAttr(el) {
-		for(var i = 0;i<el.length;i++){
-			if(el[i]['bo-if']);
+
+	var index = 0;
+	var modList = {};
+	var xhfor = 'xh-for';
+	var xhif = 'xh-if';
+	var singletag = 'input,img,link,meta,';
+
+	var regexp = /\{\{((?:.|\n)+?)\}\}/m;
+	var subtagexp = /<[^<]*/g;
+	var tagnamexp = /<\s*([a-zA-Z]+)/;
+	var endtagexp = /<\s*\/\s*(\w+)/;
+	var forexp = getAttrExp(xhfor);
+	var ifexp = getAttrExp(xhif);
+	var isArray = _is('Array');
+	win.bo = {
+		"Bo":Bo,
+		"mod2Dom":mod2Dom,
+		"parseStr":parseStr,
+		"dataInject":dataInject
+	};
+	function dataInject(text, data){
+		while(true){
+			var c = regexp.exec(text);
+			if(c === null) return text;
+			if(data[c[1]]) text = text.replace(c[0], data[c[1]]);
 		}
 	}
-	function getQueryString(str, name){
-	    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
-	    var r = str.substr(1).match(reg);
-	    if(r!==null) return decodeURIComponent(r[2]);
-	    return null;
+	function getForStr(str, obj) {
+		var tn = tagnamexp.exec(str)[1];/*get tag name*/
+		var fe = forexp.exec(str);/*for*/
+		if(fe) str = str.replace(fe[0],'');
+		var ie = ifexp.exec(str);
+		obj[index] = {
+			_par:obj,
+			_tag:tn,
+			_str:str
+		};
+		if(fe !== null) obj[index]._for = fe[1];
+		if(ie !== null) obj[index]._if = ie[1];
+		return obj[index];
 	}
-	setModelBox();
+	function getAttrExp(attr) {
+		// var main = "\\s*=\\s*('([^']*)'|\"([^\"]*)\")";
+		return new RegExp(attr+"\\s*=\\s*\"([^\"]*)\"","i");
+	}
+	function $x(el) {
+		var mel = document.querySelectorAll(el);
+		if(mel.length<2) return mel[0];
+		return mel;
+	}
+	function Bo(el, data) {
+		if(!modList[el]){
+			var inhtml = $x(el).innerHTML;
+			index = 0;
+			modList[el] = parseStr(inhtml, {});
+		}
+		$x(el).innerHTML = mod2Dom(modList[el], data);
+		return bo;
+	}
+	function parseStr(str, obj) {
+		var sub = subtagexp.exec(str);
+		// console.log(sub);
+		if(sub === null) return obj;
+		var substr = sub[0];
+		var subobj = obj._par;
+		var et = endtagexp.exec(substr);
+		if(et === null){
+			subobj = getForStr(substr, obj);
+			index += 1;
+		}
+		parseStr(str, subobj);
+		return obj;
+	}
+	function mod2Dom(mod, data) {
+		if(!data) return;
+		var html = '';
+		if(isArray(data)){
+			for (var i = 0;i<data.length;i++) {
+				html += submod2Dom(mod, data[i]);
+			}
+		}else{
+			html += submod2Dom(mod, data);
+		}
+		return html;
+	}
+	function submod2Dom(mod, data) {
+		var keys = Object.keys(mod);
+		var value = null;
+		var endtag = '';
+		var html = '';
+		for (var i = 0;i<keys.length;i++) {
+			if(keys[i].indexOf('_')>-1) continue;/*if this attr is undom continue*/
+			value = mod[keys[i]];
+			if(value._if){
+				if(!data[value._if]) continue;
+			}
+			html += dataInject(value._str, data);/*data inject into start tag*/
+			if(singletag.indexOf(value._tag+',')<0) endtag = '</'+value._tag+'>';/*if this tag is not single tag*/
+			if(value._for) data = data[value._for];/*if tag has 'for' attr*/
+			html += mod2Dom(value, data)+endtag;/*get subtag and endtag*/
+		}
+		return html;
+	}
+	function _is(str) {
+		return function (obj) {
+			return Object.prototype.toString.call(obj) === '[object '+str+']';
+		};
+	}
+	Bo('#test', testd);
 })(window, jQuery);
