f29bfd4d9ec8865896f9eaa9359adf7e4c11e982
feng <bobo@fengdeMacBook-Pro.local>
2017-11-24 11:25:17 +0800

    代码转为es6

--- js/canvas.js
+++ js/canvas.js
@@ -1,13 +1,11 @@
 (function (win) {
-	var w = win.innerWidth, h = win.innerHeight;
-	var abs = Math.abs;
-	var canvas = document.querySelector('canvas');
-	var body = document.querySelector('body');
-	var number = 0;
+	const W = win.innerWidth, H = win.innerHeight;
+	const Abs = Math.abs;
+	const canvas = document.querySelector('canvas');
 
-	canvas.width = w;
-	canvas.height = h;
-	var ctx = canvas.getContext('2d');
+	canvas.width = W;
+	canvas.height = H;
+	const ctx = canvas.getContext('2d');
 	function showNumber(num) {
 		ctx.save();
 		ctx.font = '2rem Arial';
@@ -16,7 +14,7 @@
 		ctx.restore();
 	}
 	function animate(){
-        ctx.clearRect(0, 0, w, h);
+        ctx.clearRect(0, 0, W, H);
         showNumber(number);
         requestAnimationFrame(animate);
     }
