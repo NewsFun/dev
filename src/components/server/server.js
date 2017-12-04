3fae0c555868419de6a6afaee1524ce609cda323
System Administrator <root@fengdeMacBook-Pro.local>
2017-12-04 10:25:31 +0800

    测试文件夹

--- src/components/server/server.js
+++ src/components/server/server.js
@@ -1,33 +1,49 @@
 const http = require('http');
 const fs = require('fs');
 const port = '2850';
-
 const documentRoot = './';
-var resp = null;
 
-function startServer(){
-    http.createServer(function(req, res){
-        let url = req.url;
-        let file = documentRoot + url;
-        resp = res;
-        readFiles(file);    
-    }).listen(port);
-    console.log('服务器启动成功，端口：'+port);
-}
-function readFiles(path){
-    fs.readFile(file, function(err, data){
+function readPath(path){
+    var files = fs.existsSync(path);
+    if(!files){
+        console.log('文件不存在，请确认路径正确！');
+        return;
+    }
+    fs.stat(path, function(err, stat){
         if(err){
-            serverRes(404, '<h1>404错误</h1><p>你要找的页面不存在</p>');
+            console.log(err);
+            return;
+        }
+        if(stat.isDirectory()){
+            console.log(path+':是文件夹');
         }else{
-            serverRes(200, data);
+            console.log(path+':是文件');
         }
     });
 }
-function serverRes(code, data){
-    resp.writeHeader(code,{
+function startServer(){
+    http.createServer(function(req, res){
+        let url = req.url;
+        let path = documentRoot + url;
+        readFiles(res, path);    
+    }).listen(port);
+    console.log('服务器启动成功，端口：'+port);
+}
+function readFiles(res, path){
+    fs.readFile(path, function(err, data){
+        if(err){
+            serverRes(res, 404, '<h1>404错误</h1><p>你要找的页面不存在</p>');
+        }else{
+            serverRes(res, 200, data);
+        }
+    });
+}
+function serverRes(res, code, data){
+    res.writeHeader(code,{
         'content-type' : 'text/html;charset="utf-8"'
     });
-    resp.write(data);
-    resp.end();
+    res.write(data);
+    res.end();
 }
+readPath(documentRoot);
 startServer();
