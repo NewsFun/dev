d6d9bd58c720c8416a9bf63b6d7457f6b870330f
System Administrator <root@fengdeMacBook-Pro.local>
2017-12-04 18:22:50 +0800

    简单的本地服务器程序可以访问本地文件夹及浏览器支持的文件

--- src/components/server/server.js
+++ src/components/server/server.js
@@ -1,49 +1,57 @@
 const http = require('http');
 const fs = require('fs');
+const cp = require('child_process');
+
 const port = '2850';
 const documentRoot = './';
+const res404 = '<h1>404错误</h1><p>你要找的内容不存在</p>';
 
-function readPath(path){
-    var files = fs.existsSync(path);
-    if(!files){
-        console.log('文件不存在，请确认路径正确！');
-        return;
-    }
-    fs.stat(path, function(err, stat){
-        if(err){
-            console.log(err);
-            return;
-        }
-        if(stat.isDirectory()){
-            console.log(path+':是文件夹');
+function readPath(path, res){
+    fs.stat(path, (err, stat)=>{
+        let isdir = isFolder(stat);
+        if(isdir){
+            readFileList(path, res);
         }else{
-            console.log(path+':是文件');
+            readAFile(path, res);
         }
     });
+}
+function isFolder(stat){
+    return stat.isDirectory();
+}
+function fileError(){
+    console.log('文件不存在，请确认路径正确！');
+}
+function readFileList(path, res){
+    let filelist = fs.readdirSync(path);
+    let list = '';
+    filelist.forEach(function(item, index){
+        list += '<div><a href="./'+item+'">'+item+'</a></div>';
+    });
+    serverRes(res, 200, list);
 }
 function startServer(){
     http.createServer(function(req, res){
         let url = req.url;
         let path = documentRoot + url;
-        readFiles(res, path);    
+        readPath(path, res);
     }).listen(port);
     console.log('服务器启动成功，端口：'+port);
 }
-function readFiles(res, path){
-    fs.readFile(path, function(err, data){
+function readAFile(path, res){
+    fs.readFile(path, 'UTF-8', function(err, data){
         if(err){
-            serverRes(res, 404, '<h1>404错误</h1><p>你要找的页面不存在</p>');
+            console.log('读取文件错误，请确认地址有效');
         }else{
             serverRes(res, 200, data);
         }
     });
 }
-function serverRes(res, code, data){
+function serverRes(res, code = 404, data = res404){
     res.writeHeader(code,{
         'content-type' : 'text/html;charset="utf-8"'
     });
     res.write(data);
     res.end();
 }
-readPath(documentRoot);
 startServer();
