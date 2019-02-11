36f789058b7e8eeae29943a323dc2bd673cea106
bobo <80733049@qq.com>
2019-02-11 12:11:37 +0800

    change project into vue-ui, changed some configs

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true
+  },
+  extends: ['plugin:vue/essential', 'standard'],
+  plugins: ['vue'],
+  // rules: {
+  //   'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  //   'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  //   'space-before-function-paren': 'off',
+  //   'semi': 'off',
+  //   'indent': 'off',
+  //   'object-curly-spacing': 'off',
+  //   'quotes': 'off',
+  //   'no-eval': 'off'
+  // },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+};
