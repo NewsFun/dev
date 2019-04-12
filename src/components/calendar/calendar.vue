4b54cd7eafe341ebf45e3d3feac5c07642bb3fff
fenghengbao <fenghengbao@koolearn-inc.com>
2019-04-12 16:49:43 +0800

    backup

--- src/components/calendar/calendar.vue
+++ src/components/calendar/calendar.vue
@@ -1,23 +1,21 @@
 <template>
-  <div class="calendar-wrap">
-    <div class="calendar">
-      <div class="calendar-title">
-        <div class="prevMouth" :class="isNextClass" @click="prevMonth"></div>
-        <span class="currentDate">
-          <span>{{ monthSign }}</span>
-          {{ currentYear }}
-        </span>
-        <div class="nextMouth" :class="isPrevClass" @click="nextMonth"></div>
-      </div>
-      <div class="calendar-body">
-        <ul class="week">
-          <li v-for="(item,index) in weekSign" :key="index">{{item}}</li>
-        </ul>
-        <ul class="day">
-          <li v-for="i in firstDay" :key="i"></li>
-          <li v-for="(item, j) in monthDays" :key="j">{{item}}</li>
-        </ul>
-      </div>
+  <div class="calendar">
+    <div class="calendar-title">
+      <div class="prevMouth" :class="isNextClass" @click="prevMonth"></div>
+      <span class="currentDate">
+        <span>{{ monthSign }}</span>
+        {{ currentYear }}
+      </span>
+      <div class="nextMouth" :class="isPrevClass" @click="nextMonth"></div>
+    </div>
+    <div class="calendar-body">
+      <ul class="week">
+        <li v-for="(item, i) in weekSign" :key="i" class="eday">{{item}}</li>
+      </ul>
+      <ul class="day">
+        <li v-for="i in firstDay" :key="i" class="eday"></li>
+        <li v-for="(item, i) in monthDays" :key="'d'+i" class="eday">{{item}}</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -52,16 +50,7 @@ export default {
       highlightArr: [],
       weekSign: ["S", "M", "T", "W", "T", "F", "S"],
       isNext: false,
-      isPrev: true,
-      currentDay: "",
-      totalSignDay: 0,
-      continueSignDay: 0,
-      isSign: false,
-      isTodaySign: {
-        year: YEAR,
-        month: MONTH,
-        day: DAY
-      }
+      isPrev: true
     };
   },
   computed: {
@@ -83,52 +72,27 @@ export default {
       return new Date(this.currentYear, this.currentMonth, 1).getDay();
     }
   },
-  created() {
-    // this.dateFill();
+  props: {
+    date: {
+      type: String,
+      default: ''
+    }
   },
   methods: {
     nextMonth() {
-      if (this.isNext) {
-        if (this.currentMonth > 10) {
-          this.currentYear++;
-          this.currentMonth = 0;
-        } else {
-          this.currentMonth++;
-        }
+      if (this.currentMonth > 10) {
+        this.currentYear++;
+        this.currentMonth = 0;
+      } else {
+        this.currentMonth++;
       }
-      this.isNextFun();
     },
     prevMonth() {
-      if (this.isPrev) {
-        if (this.currentMonth < 1) {
-          this.currentYear--;
-          this.currentMonth = 11;
-        } else {
-          this.currentMonth--;
-        }
-      }
-      this.isPrevFun();
-    },
-    isNextFun() {
-      // 判断还有没有下个月
-      if (this.currentYear >= new Date().getFullYear()) {
-        if (this.currentMonth === new Date().getMonth()) {
-          this.isNext = false;
-          this.isPrev = true;
-        }
+      if (this.currentMonth < 1) {
+        this.currentYear--;
+        this.currentMonth = 11;
       } else {
-        this.isNext = true;
-        this.isPrev = true;
-      }
-    },
-    isPrevFun() {
-      // 判断还有没有上个月
-      if (this.currentYear === 2017 && this.currentMonth === 8) {
-        this.isPrev = false;
-        this.isNext = true;
-      } else {
-        this.isPrev = true;
-        this.isNext = true;
+        this.currentMonth--;
       }
     }
   }
