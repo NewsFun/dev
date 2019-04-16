91198ee8c134315e9d2499e9698fa9cc4adc7925
fenghengbao <fenghengbao@koolearn-inc.com>
2019-04-16 11:32:18 +0800

    backup

--- src/components/calendar/calendar.vue
+++ src/components/calendar/calendar.vue
@@ -14,7 +14,12 @@
       </ul>
       <ul class="day">
         <li v-for="i in firstDay" :key="i" class="eday"></li>
-        <li v-for="(item, i) in monthDays" :key="'d'+i" class="eday">{{item}}</li>
+        <li
+          v-for="(item, i) in monthDays"
+          :key="'d'+i"
+          class="eday"
+          @click="onChangeDate(item)"
+        >{{item}}</li>
       </ul>
     </div>
   </div>
@@ -45,11 +50,17 @@ const DAY = DATE.getDate();
 export default {
   data() {
     return {
+      // 当前年份
       currentYear: YEAR,
+      // 当前月份
       currentMonth: MONTH,
-      highlightArr: [],
+      // 当前日期
+      currentDate: 1,
+      // 一周标示
       weekSign: ["S", "M", "T", "W", "T", "F", "S"],
+      // 是否下一页
       isNext: false,
+      // 是否上一页
       isPrev: true
     };
   },
@@ -62,26 +73,44 @@ export default {
       if (this.isPrev) return "";
       return "off";
     },
+    // 月份标示，默认英文标
     monthSign() {
       return MONTH_SIGN[this.currentMonth];
     },
+    // 一个月的天数
     monthDays() {
       return new Date(this.currentYear, this.currentMonth, 0).getDate();
     },
+    // 当月第一天的礼拜数
     firstDay() {
       return new Date(this.currentYear, this.currentMonth, 1).getDay();
+    },
+    // 当前日期
+    resultDate() {
+      let result = [this.currentYear, this.currentMonth + 1, this.currentDate];
+      if (this.connectType) return result.join(this.connectType)
+      return result
     }
   },
   props: {
     date: {
       type: String,
-      default: ''
+      default: ""
+    },
+    connectType: {
+      type: String,
+      default: ""
     },
     handleChangeDate: {
       type: Function,
       default() {
-        return () => {}
+        return () => {};
       }
+    }
+  },
+  watch: {
+    resultDate(val) {
+      this.handleChangeDate && this.handleChangeDate(val);
     }
   },
   methods: {
@@ -100,6 +129,9 @@ export default {
       } else {
         this.currentMonth--;
       }
+    },
+    onChangeDate(date) {
+      this.currentDate = date;
     }
   }
 };
--- src/components/toast/README.md
+++ src/components/toast/README.md
@@ -1,16 +1,35 @@
-### toast插件应用方法
-1.在main.js中引入当前toast.js
-2.在main.js中引入当前样式toast.scss
-3.Vue.use()
-###toast插件有2种功能
-1.Vue.prototype.$toast(tips,config)
-      tips---toast传入的信息
-      config---toast插件的配置项，目前有3个参数
-          duration---toast展示后duration毫秒小时
-          width---toast展示宽度
-          closeType---toast关闭方式：参数值为'manual'时需手动关闭，调用
-          Vue.prototype.$toast.close();参数值为其他值时无效；
-                
-2.Vue.prototype.$loading(tip)默认不会自动关闭，需手动触发
-      tips---loading图标下方展示的文字
-      Vue.prototype.$loading.close() 关闭loading
+# toast插件应用方法
+
+main.js
+
+```javascript
+import toast from 'toast.js'
+import 'toast.scss'
+
+Vue.use(toast)
+```
+
+## toast插件有2种功能
+
+### 弹窗提示
+
+```javascript
+// tips:传入的信息
+// config:插件的配置项，目前有3个参数
+// duration:展示后duration毫秒小时
+// width:展示宽度
+// closeType:关闭方式(参数值为'manual'时需手动关闭,调用close方法)
+Vue.prototype.$toast(tips,config)
+// 参数值为其他值时无效;
+Vue.prototype.$toast.close();
+```
+
+### 加载动画
+
+```javascript
+// 默认不会自动关闭，需手动触发
+// tips:loading图标下方展示的文字
+Vue.prototype.$loading(tip)
+// 关闭loading
+Vue.prototype.$loading.close()
+```
