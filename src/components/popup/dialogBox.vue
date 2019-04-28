2160a2cba2f4f74349c3fb34743329728b561ffd
fenghengbao <fenghengbao@koolearn-inc.com>
2019-04-28 15:40:36 +0800

    debug: can't move left and right

--- src/components/popup/dialogBox.vue
+++ src/components/popup/dialogBox.vue
@@ -1,114 +1,121 @@
 <template>
-    <div class="dialog-container" v-if="popShow">
-        <div class="dialog-cover"></div>
-        <div class="dialog-wrap">
-            <div class="dialog-head dialog-box" v-if="showTitle">
-                <span class="title">{{title}}</span>
-                <span class="close" @click="onClose">×</span>
-            </div>
-            <div class="dialog-body">
-                <slot></slot>
-            </div>
-            <div class="dialog-footer">
-                <span class="dialog-btn" :class="btnClass" v-if="showCancelBtn" @click="onCancel">{{cancelText}}</span>
-                <span class="dialog-btn" :class="btnClass" v-if="showConfirmBtn" @click="handleConfirm">{{confirmText}}</span>
-            </div>
-        </div>
+  <div class="dialog-container" v-if="popShow">
+    <div class="dialog-cover"></div>
+    <div class="dialog-wrap">
+      <div class="dialog-head dialog-box" v-if="showTitle">
+        <span class="title">{{title}}</span>
+        <span class="close" @click="onClose">×</span>
+      </div>
+      <div class="dialog-body">
+        <slot></slot>
+      </div>
+      <div class="dialog-footer">
+        <span
+          class="dialog-btn"
+          :class="btnClass"
+          v-if="showCancelBtn"
+          @click="onCancel"
+        >{{cancelText}}</span>
+        <span
+          class="dialog-btn"
+          :class="btnClass"
+          v-if="showConfirmBtn"
+          @click="handleConfirm"
+        >{{confirmText}}</span>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'messageBox',
-    data() {
-        return {
-            popShow: false
-        }
+  name: "messageBox",
+  data() {
+    return {
+      popShow: false
+    };
+  },
+  props: {
+    show: {
+      type: Boolean,
+      default: false
     },
-    props: {
-        show: {
-            type: Boolean,
-            default: false
-        },
-        title: {
-            type: String,
-            default: '提示'
-        },
-        showTitle: {
-            type: Boolean,
-            default: true
-        },
-        cancelText: {
-            type: String,
-            default: '取消'
-        },
-        confirmText: {
-            type: String,
-            default: '确认'
-        },
-        content: {
-            type: String,
-            default: ''
-        },
-        showCancelBtn: {
-            type: Boolean,
-            default: true
-        },
-        showConfirmBtn: {
-            type: Boolean,
-            default: true
-        },
-        handleClose: {
-            type: Function,
-            default() {
-                return () => {
-                }
-            }
-        },
-        handleCancel: {
-            type: Function,
-            default() {
-                return () => {
-                }
-            }
-        },
-        handleConfirm: {
-            type: Function,
-            default() {
-                return () => {
-                }
-            }
-        },
-        buttonType: {
-            type: String,
-            default: 'normal'
-        }
+    title: {
+      type: String,
+      default: "提示"
     },
-    computed: {
-        btnClass() {
-            return this.buttonType === 'solid' ? 'on' : ''
-        }
+    showTitle: {
+      type: Boolean,
+      default: true
     },
-    watch: {
-        show(val) {
-            this.popShow = val
-        }
+    cancelText: {
+      type: String,
+      default: "取消"
     },
-    mounted() {
-        this.popShow = this.show
+    confirmText: {
+      type: String,
+      default: "确认"
     },
-    methods: {
-        onClose() {
-            this.popShow = false
-            this.handleClose && this.handleClose()
-        },
-        onCancel() {
-            this.popShow = false
-            this.handleCancel && this.handleCancel()
-        }
+    content: {
+      type: String,
+      default: ""
+    },
+    showCancelBtn: {
+      type: Boolean,
+      default: true
+    },
+    showConfirmBtn: {
+      type: Boolean,
+      default: true
+    },
+    handleClose: {
+      type: Function,
+      default() {
+        return () => {};
+      }
+    },
+    handleCancel: {
+      type: Function,
+      default() {
+        return () => {};
+      }
+    },
+    handleConfirm: {
+      type: Function,
+      default() {
+        return () => {};
+      }
+    },
+    buttonType: {
+      type: String,
+      default: "normal"
     }
-}
+  },
+  computed: {
+    btnClass() {
+      return this.buttonType === "solid" ? "on" : "";
+    }
+  },
+  watch: {
+    show(val) {
+      this.popShow = val;
+    }
+  },
+  mounted() {
+    this.popShow = this.show;
+  },
+  methods: {
+    onClose() {
+      this.popShow = false;
+      this.handleClose && this.handleClose();
+    },
+    onCancel() {
+      this.popShow = false;
+      this.handleCancel && this.handleCancel();
+    }
+  }
+};
 </script>
 <style lang="scss">
-  @import "./css/dialog-box.scss";
+@import "./css/dialog-box.scss";
 </style>
--- src/components/popup/messageBox.vue
+++ src/components/popup/messageBox.vue
@@ -1,119 +1,127 @@
 <template>
-    <div class="msgbox-container" v-if="popShow">
-        <div class="msgbox-cover"></div>
-        <div class="msgbox-wrap">
-            <div class="msgbox-head msgbox-box" v-if="showTitle">
-                <span class="title">{{title}}</span>
-                <span class="close" @click="close">×</span>
-            </div>
-            <div class="msgbox-body msgbox-box">
-                <div :class="contentClass" v-html="content"></div>
-            </div>
-            <div class="msgbox-footer">
-                <span class="msgbox-btn" :class="btnClass" v-if="showCancelBtn" @click="onCancel">{{cancelText}}</span>
-                <span class="msgbox-btn" :class="btnClass" v-if="showConfirmBtn" @click="handleConfirm">{{confirmText}}</span>
-            </div>
-        </div>
+  <div class="msgbox-container" v-if="popShow">
+    <div class="msgbox-cover"></div>
+    <div class="msgbox-wrap">
+      <div class="msgbox-head msgbox-box" v-if="showTitle">
+        <span class="title">{{title}}</span>
+        <span class="close" @click="close">×</span>
+      </div>
+      <div class="msgbox-body msgbox-box">
+        <div :class="contentClass">{{content}}</div>
+      </div>
+      <div class="msgbox-footer">
+        <span
+          class="msgbox-btn"
+          :class="btnClass"
+          v-if="showCancelBtn"
+          @click="onCancel"
+        >{{cancelText}}</span>
+        <span
+          class="msgbox-btn"
+          :class="btnClass"
+          v-if="showConfirmBtn"
+          @click="handleConfirm"
+        >{{confirmText}}</span>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-/* eslint-disable */
 export default {
-    name: 'messageBox',
-    data() {
-        return {
-            popShow: false
-        }
+  name: "messageBox",
+  data() {
+    return {
+      popShow: false
+    };
+  },
+  props: {
+    show: {
+      type: Boolean,
+      default: false
     },
-    props: {
-        show: {
-            type: Boolean,
-            default: false
-        },
-        title: {
-            type: String,
-            default: '提示'
-        },
-        showTitle: {
-            type: Boolean,
-            default: true
-        },
-        cancelText: {
-            type: String,
-            default: '取消'
-        },
-        confirmText: {
-            type: String,
-            default: '确认'
-        },
-        content: {
-            type: String,
-            default: ''
-        },
-        center: {
-            type: Boolean,
-            default: false
-        },
-        showCancelBtn: {
-            type: Boolean,
-            default: true
-        },
-        showConfirmBtn: {
-            type: Boolean,
-            default: true
-        },
-        handleCancel: {
-            type: Function,
-            default() {
-                return () => {}
-            }
-        },
-        handleConfirm: {
-            type: Function,
-            default() {
-                return () => {}
-            }
-        },
-        handleClose: {
-            type: Function,
-            default() {
-                return () => {}
-            }
-        },
-        btnType: {
-            type: String,
-            default: 'normal'
-        }
+    title: {
+      type: String,
+      default: "提示"
     },
-    computed: {
-        btnClass() {
-            return this.btnType === 'solid' ? 'on' : '';
-        },
-        contentClass() {
-            return this.center && 'center';
-        }
+    showTitle: {
+      type: Boolean,
+      default: true
     },
-    watch: {
-        show(val) {
-            this.popShow = val
-        }
+    cancelText: {
+      type: String,
+      default: "取消"
     },
-    mounted() {
-        this.popShow = this.show
+    confirmText: {
+      type: String,
+      default: "确认"
     },
-    methods: {
-        close() {
-            this.popShow = false
-            this.handleClose && this.handleClose()
-        },
-        onCancel() {
-            // this.popShow = false
-            this.handleCancel && this.handleCancel()
-        }
+    content: {
+      type: String,
+      default: ""
+    },
+    center: {
+      type: Boolean,
+      default: false
+    },
+    showCancelBtn: {
+      type: Boolean,
+      default: true
+    },
+    showConfirmBtn: {
+      type: Boolean,
+      default: true
+    },
+    handleCancel: {
+      type: Function,
+      default() {
+        return () => {};
+      }
+    },
+    handleConfirm: {
+      type: Function,
+      default() {
+        return () => {};
+      }
+    },
+    handleClose: {
+      type: Function,
+      default() {
+        return () => {};
+      }
+    },
+    btnType: {
+      type: String,
+      default: "normal"
     }
-}
+  },
+  computed: {
+    btnClass() {
+      return this.btnType === "solid" ? "on" : "";
+    },
+    contentClass() {
+      return this.center && "center";
+    }
+  },
+  watch: {
+    show(val) {
+      this.popShow = val;
+    }
+  },
+  mounted() {
+    this.popShow = this.show;
+  },
+  methods: {
+    close() {
+      this.popShow = false;
+      this.handleClose && this.handleClose();
+    },
+    onCancel() {
+      this.handleCancel && this.handleCancel();
+    }
+  }
+};
 </script>
 <style lang="scss">
-  @import "./css/msg-box.scss";
+@import "./css/msg-box.scss";
 </style>
